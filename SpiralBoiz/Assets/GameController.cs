1f18f40b2d241a8a9865e9d3e084a9a9bd534cbc
Huw Tingley <Huw.Tingley@gmail.com>
2018-09-27 12:37:29 +0100

    Starting work on main menu

---
+++ SpiralBoiz/Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int no_players = 0;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+}
