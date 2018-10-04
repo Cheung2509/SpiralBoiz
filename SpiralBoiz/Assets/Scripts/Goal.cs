0888aedf947449be03ebaf7628dace984c2d8f2e
Huw Tingley <Huw.Tingley@gmail.com>
2018-10-04 13:09:46 +0100

    Goal explosions and reset

--- SpiralBoiz/Assets/Scripts/Goal.cs
+++ SpiralBoiz/Assets/Scripts/Goal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
@@ -9,6 +10,15 @@ public class Goal : MonoBehaviour
 
     private int count = 0;
     public Text scoreText;
+    public Text hasScoredText;
+
+    [SerializeField]
+    private String scored_colour_name;
+
+    [SerializeField]
+    private Color ball_explosion_colour;
+
+    private float reset_time = 3.0f;
 
 	// Use this for initialization
 	void Start ()
@@ -28,7 +38,36 @@ public class Goal : MonoBehaviour
         {
             //Increment score here
             Debug.Log("GOAL!!!!!");
+
+            //Goal explosion
+            ParticleSystem ball_ps = collision.GetComponent<ParticleSystem>();
+            var main = ball_ps.main;
+            main.startColor = ball_explosion_colour;
+
+            collision.GetComponent<ParticleSystem>().Play();
+
+            collision.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            collision.GetComponent<SpriteRenderer>().color = Color.clear;
+
+            //stops it colliding with player after goal scored
+            collision.GetComponent<CircleCollider2D>().isTrigger = true;
+
+            StartCoroutine(GoalScored(collision.gameObject));
+
             count++;
         }
+    }
+
+    IEnumerator GoalScored(GameObject ball)
+    {
+        hasScoredText.text = scored_colour_name + " HAS SCORED";
+        hasScoredText.color = ball_explosion_colour;
+
+        yield return new WaitForSeconds(reset_time);
+        hasScoredText.color = Color.clear;
+
+        ball.transform.position = Vector3.zero;
+        ball.GetComponent<SpriteRenderer>().color = Color.white;
+        ball.GetComponent<CircleCollider2D>().isTrigger = false;
     }
 }
