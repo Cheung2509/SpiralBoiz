86df428b94f3b96588d89266ce684694d4ae80ae
Luke <luke4.roberts@live.uwe.ac.uk>
2018-10-08 17:37:05 +0100

    started walls script

---
+++ SpiralBoiz/Assets/Scripts/Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+        // get car rigidbody
+        Rigidbody2D carRB = collision.collider.gameObject.GetComponent<Rigidbody2D>();
+
+        // get car velocity
+        Vector2 carVelocity = carRB.velocity;
+
+        // reflect velocity
+        Vector2 reflect = Vector2.Reflect(carVelocity, collision.contacts[0].normal);
+    }
+}
